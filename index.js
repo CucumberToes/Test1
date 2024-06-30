e3189dcd4634292b3edee5e8e7d833c83cb185c0
ColeAT <49347013+CucumberToes@users.noreply.github.com>
2024-06-30 19:29:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/submit-email', (req, res) => {
+    const email = req.body.email;
+    console.log('Received email:', email);
+    // You can add code here to save the email to a database or perform other actions
+    res.json({ message: 'Email received successfully' });
 });
 
 app.listen(port, () => {
